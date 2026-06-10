3def5928cc2717e1601887f23d2c1454af5e0076
t425062-a11y <t425062@m.chukyo-u.ac.jp>
2026-06-10 12:00:16 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const app = express();
+
+const http = require("http").createServer(app);
+
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+
+    console.log("接続");
+
+    socket.on("gameState", (data) => {
+
+        socket.broadcast.emit(
+            "enemyState",
+            data
+        );
+
+    });
+
+    socket.on("disconnect", () => {
+
+        console.log("切断");
+
+    });
+
+});
+
+http.listen(8080, "0.0.0.0", () => {
+
+    console.log("Server Start");
+
+});
